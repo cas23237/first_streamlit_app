d67ac9e7eed043960f01a0719d03267275e6791f
cas23237 <155842305+cas23237@users.noreply.github.com>
2024-01-06 16:47:23 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,6 +34,7 @@ my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+my_cur.execute("insert into fruit_load_list add_my_fruit")
 streamlit.write('Thanks for adding ', add_my_fruit)
 
 
